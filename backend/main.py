9c28f25d3971c6f7a52fe1841de90b4a9e239943
Anish Saha <aksaha43825@kgpian.iitkgp.ac.in>
2026-06-02 12:45:15 +0530

    fix: update CORS settings to allow all origins for broader access

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(redirect_slashes=False)
 # CORS (must be here)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://lost--n--found.vercel.app"],  # for development
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
